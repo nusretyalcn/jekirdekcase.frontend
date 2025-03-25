20aed3cdb65578da61dd384de0be65178ced87c3
Nusret Yalçın <yalcnusret@gmail.com>
2025-03-26 00:38:30 +0300

    CustomerPage Added Used table from and filter component For responsive design CustomerPage.css added CustomerService added for backend request Added rooting for Customer

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
+import CustomersPage from "../pages/CustomersPage";
 
 const AppRouter = () => {
   return (
@@ -8,6 +9,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/customers" element={<CustomersPage />} />
       </Routes>
     </Router>
   );
